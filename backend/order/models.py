40bbbe677a04be506e8b50713d0e3e181a7684b7
super <nguyentuanvuabc@gmail.com>
2023-06-04 22:40:57 +0700

    ss

---
+++ backend/order/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from product.models import Product
+from cart.models import CartItem
+
+class Order(models.Model):
+    user = models.ForeignKey('authentication.AppUser', related_name='orders', on_delete=models.CASCADE,)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    quantity = models.PositiveIntegerField(default=1)
+    purchased_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    @classmethod
+    def create_from_cart_item(cls, cartItem : CartItem, order:Order):
+        return cls(product=cartItem.product, purchased_price=cartItem.product.price,quantity=cartItem.quantity, order=order )
